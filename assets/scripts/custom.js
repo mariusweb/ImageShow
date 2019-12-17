143f3ae42601f606660b676d815cb3d2a803fe9c
mariusweb <39734577+mariusweb@users.noreply.github.com>
2019-12-17 23:28:21 +0200

    Added function to display photo and information

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,25 +1,19 @@
 (function(){
 
 "use strict";
-	
-	
-	fetch("https://picsum.photos/v2/list").then(function(resp){
-			return resp.json();
-		}).then(function (data){
-			// for(let i = 0; i < data.length; i++){
-			// 	console.log(data[i].download_url)
-			// }
-			let dataImages = data.map( (data) => {
-				return `
-				<div class="image">
-					<img class="photo" src="${"https://picsum.photos/id/"+data.id+"/200"}">
-				</div>
-				`
-			}).join('');
-			document.querySelector(".images").innerHTML = dataImages;
-			console.log(dataImages);
-		});
-		
-		
 
+	fetch("https://picsum.photos/v2/list")
+	.then(function(resp){
+		return resp.json();
+	}).then(function (data){
+		let smallImages = data.map( (data) => {
+			let image = `
+			<div class="small-image">
+				<img class="photo" id="${data.id}" src="https://picsum.photos/id/${data.id}/200" onclick="loadFullPhoto(this.id)">
+			</div>
+			`
+			return image
+		}).join('');
+		document.querySelector(".images").innerHTML = smallImages;
+	});	
 }());
